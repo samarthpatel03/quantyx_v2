f0f7c81db643164a78c445bc969a273758df9b65
Samarth Patel <samarthpatel6174@gmail.com>
2026-05-06 14:00:33 +0530

    Added frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: ["class"],
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+module.exports= {
+  content: ["./index.html", "./src/**/*.{js,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
